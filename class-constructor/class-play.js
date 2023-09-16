6674d6895097143ea5a9665efd7dab51f8b8b74f
Phil Taylor <phil.c.taylor@gmail.com>
2023-09-15 22:45:10 -0400

    set up script

---
+++ class-constructor/class-play.js
@@ -0,0 +1,21 @@
+// using https://javascript.info/class
+
+// Define what a book is - 'author', 'title', 'number of pages', 
+// 'whether it's been read' -- anything else you want
+
+// Create an array to store books and put the books in
+
+// Take user input on books to add to book array
+
+// Display each book on a page in a table
+
+// Add a 'new book' button 
+
+// The 'new book' button brings up a form with 'author', 'title', 
+//'number of pages', 'whether it's been read' -- anything else you want
+
+// Display the form in a modal overlay
+
+// Add a button on each book's display to delete it
+
+// Add a button on each book's display to change its read status
